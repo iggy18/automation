516fb30bfcfe7c7b23d268ca53bac741652d9f42
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2020-12-04 08:47:27 -0800

    got phone numbers but something is wrong with email regex

--- automation.py
+++ automation.py
@@ -1,7 +1,16 @@
-with open("potential_contacts.txt", "r") as f:
-    file = f.read()
-    regex = "\w+(\.\w+)*@\w+\.\w+"
+import re
 
 with open("potential_contacts.txt", "r") as f:
-    file = f.read()
-    regex = "\(?\d{3}\)?[-\s]?\d{3}[-\s]?\d{4}"
+    emails_in_file = f.read()
+    regex = r"\w+(\.\w+)*@\w+\.\w+"
+    email = re.findall(regex, emails_in_file)
+    sorted_email = email.sort()
+    print(email)
+
+
+with open("potential_contacts.txt", "r") as f:
+    phone_numbers_in_file = f.read()
+    regex = r"\(?\d{3}\)?[-\s]?\d{3}[-\s]?\d{4}"
+    phone_numbers = re.findall(regex, phone_numbers_in_file)
+    sorted_phone_numbers = phone_numbers.sort()
+    print(phone_numbers)
